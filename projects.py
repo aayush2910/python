e9c21302b3c7eef706c9a7a136fcb29face49b78
aayush2910 <fularaaayush2910@gmail.com>
2023-07-14 21:15:26 +0530

    project

--- projects.py
+++ projects.py
@@ -105,12 +105,62 @@
 # far=cel_to_far(0)
 # print(far)
 
-def print_stars(n):
-    """This Function prints stars"""
-    i=0
-    while i<=n:
-        print('*'*(n-i))
-        i+=1
 
 
-print_stars(3)        
+#     """This Function prints stars"""
+# def print_stars(n):
+
+#     i=0
+#     while i<=n:
+#         print('*'*(n-i))
+#         i+=1
+
+
+# print_stars(3)        
+
+
+#THIS IS THE ROCK, PAPER AND SCIESOR GAME
+
+from random import randint
+
+print("Opponent's turn: Rock(R) , Paper(P) and Sciesor(S) :  \n")
+rand_no= randint(1,3)
+if rand_no == 1:
+    Opponent ='R'
+elif rand_no==2:
+    Opponent='S'
+else :
+    Opponent = 'P'
+
+you=input("Your turn :\nRock(R) \nPaper(P) \nSciesor(S) : \n ")
+
+def win_or_lose(player_1,your_guess):
+    if player_1 == your_guess:
+        print('Match is tie!')
+    elif (player_1=='R'):
+        if your_guess == 'S':
+            print ('You lose! ')
+        elif your_guess == 'P':
+            print ("Congratulations You won!")
+    elif player_1 == 'P':
+        if your_guess == 'R':
+            print ('You lose! ')
+        elif your_guess == 'S':
+            print ("Congratulations You won!")
+    elif player_1 == 'S':#scissor case
+        if your_guess == 'P':
+            print ('You lose! ')
+        elif your_guess == 'R':
+            print ("Congratulations You won!")
+
+print(f"\nOpponent choose :{Opponent} ")
+print(f"you choose :{you}\n")
+
+win_or_lose(Opponent,you)
+
+
+
+
+
+
+
